7b3b97b2199fc0feae4cebab3ba88e7a5bda0f76
Binu <binuandrews@gmail.com>
2025-03-02 00:41:24 +0530

    test

---
+++ mymodules/GlobalCalcModules.py
@@ -0,0 +1,49 @@
+
+
+def addTwoNumbers(num1, num2):
+  return num1 + num2
+
+def addAllNumbers(myList):
+  sum = 0
+  for item in myList:
+    sum+=item
+  print("Print before return")
+  return sum
+
+
+def printAllNumbers(*myList):
+  for item in myList:
+    print(item)
+
+
+def checkifNumeric(*numList):
+  """ This is checking whether all the params are integers of not. If any param is not integer, return False."""
+  for item in numList:
+    print(item)
+    if not isinstance(item, int):
+      return False
+  return True
+
+def findMinimum(myList, i):
+  minValue = myList[i]
+  idxMin = i
+  for x in range(i, len(myList)):
+    if myList[x] < minValue:
+      minValue = myList[x]
+      idxMin = x
+  return idxMin
+
+
+def swapValues(myList, i, j):
+  tmp = myList[i]
+  myList[i] = myList[j]
+  myList[j] = tmp
+  return myList
+
+
+def sortList(myList):
+  for i in range(0, len(myList)):
+    idxMin = findMinimum(myList, i)
+    swapValues(myList, i, idxMin)
+  return myList
+
